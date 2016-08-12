4383617dd8297de5a141ff5fd6945e4551e9ce73
KimWiese <kim.wiese@42digital.de>
2016-08-12 08:59:55 +0200

    Update und so

--- app/src/main/java/com/example/android/hausarbeitsplaner/MainActivity.java
+++ app/src/main/java/com/example/android/hausarbeitsplaner/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.hausarbeitsplaner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitMuellsack(View view){
+
+    }
+
+    public void submitPutzen(View view){
+
+    }
+
+    public void submitDailyBudget(View view){
+
+    }
+
+    public void submitEinkaufen(View view){
+
     }
 }
